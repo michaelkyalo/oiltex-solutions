d6f4f43f483c1359006f186acf0889e04871a4c1
michaelkyalo <michaelkyalo876@gmail.com>
2025-10-21 12:02:41 +0300

    footeer

---
+++ vite-project/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} Oiltex Solutions. All Rights Reserved.</p>
+      <p>Fuel Delivery | Fast | Reliable | Affordable</p>
+    </footer>
+  );
+}
+
+export default Footer;
