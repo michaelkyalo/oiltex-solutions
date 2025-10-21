4ed9f0c420c034a6a4087fd68afd6375c9b14831
michaelkyalo <michaelkyalo876@gmail.com>
2025-10-21 11:52:09 +0300

    heroes

---
+++ vite-project/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import './Hero.css'
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-text">
+        <h2>Fuel Delivered to Your Doorstep</h2>
+        <p>Fast, safe & convenient energy delivery for homes, vehicles and businesses.</p>
+        <button>Order Fuel</button>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
