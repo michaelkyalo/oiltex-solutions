e9df7e70a2f776032eb4ab97e454751d34cea8b1
michaelkyalo <michaelkyalo876@gmail.com>
2025-10-21 12:08:10 +0300

    contact

---
+++ vite-project/src/components/pages/services.jsx
@@ -0,0 +1,14 @@
+function Services() {
+  return (
+    <div className="page">
+      <h2>Our Services</h2>
+      <ul>
+        <li>Vehicle Fuel Delivery</li>
+        <li>Home Generator Fuel Supply</li>
+        <li>Industrial / Fleet Fueling</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Services;
