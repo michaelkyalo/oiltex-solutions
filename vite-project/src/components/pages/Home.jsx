15cdd4141ed514b40c1302c2f40979ba4f0ace77
michaelkyalo <michaelkyalo876@gmail.com>
2025-10-21 12:00:22 +0300

    navbar

---
+++ vite-project/src/components/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from '../components/Hero'
+
+function Home() {
+  return (
+    <div>
+      <Hero />
+      <section className="content">
+        <h2>Why Choose Oiltex?</h2>
+        <p>We deliver high-quality fuel directly to you anytime, anywhere.</p>
+      </section>
+    </div>
+  )
+}
+
+export default Home
