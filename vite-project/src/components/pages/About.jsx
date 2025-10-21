a846fec4dd9a2620a175c97cf42648a7008ac80e
michaelkyalo <michaelkyalo876@gmail.com>
2025-10-21 12:08:52 +0300

    about

--- vite-project/src/components/pages/About.jsx
+++ vite-project/src/components/pages/About.jsx
@@ -1,1 +1,11 @@
+function About() {
+  return (
+    <div className="page">
+      <h2>About Oiltex Solutions</h2>
+      <p>Oiltex is a fuel delivery company committed to making energy accessible and efficient.</p>
+    </div>
+  );
+}
+
+export default About;
 
