70193e48f4e79a44ae5ee12fb0686b64d17bd4ab
michaelkyalo <michaelkyalo876@gmail.com>
2025-10-21 11:53:37 +0300

    navbar

---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="logo">OILTEX</h1>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
