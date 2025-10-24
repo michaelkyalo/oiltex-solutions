50f027186da21fa6e53772e7cd730323fe91b853
michaelkyalo <michaelkyalo876@gmail.com>
2025-10-24 14:11:17 +0300

    anychanges

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,7 +5,7 @@ import Home from './components/pages/Home'
 import About from './components/pages/About'
 import Services from './components/pages/services'
 import Contact from './components/pages/Contact'
-import VideoSection from "./components/VideoSection";
+
 
 function App() {
   return (
